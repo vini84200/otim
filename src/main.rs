858aeffbf4a70ae0077429012c81d642c7ad2319
Pedro Colle <pedro.h.b.colle@gmail.com>
2023-08-09 21:16:25 -0300

    reading input and input files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
+use std::io;
+
+
+fn main() -> io::Result<()> {
     println!("Hello, world!");
+	let mut file_lines = io::stdin().lines();
+	let n: i32 = file_lines.next().expect("value of file").unwrap().parse().unwrap();
+	let values: Vec<i32> = file_lines.into_iter()
+		.map(|v| v.unwrap().parse().unwrap())
+		.collect::<Vec<i32>>();
+
+	
+	println!("n: {}\nvalues:{:?}",n, values);
+
+	Ok(())
 }
