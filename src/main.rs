c02b8b4083bb03f16daba75585db6dc08bc13e96
Vinicius J. F <www.vini84200@hotmail.com>
2023-08-10 20:36:51 -0300

    Refatorado construtor de solucao para modulo separado

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,28 @@
 use std::io;
 
+use crate::scheduling::Scheduling;
+
+mod scheduling;
 
 fn main() -> io::Result<()> {
-	let mut file_lines = io::stdin().lines();
-	let n: usize = file_lines.next().expect("value of file").unwrap().parse().unwrap();
-	let values: Vec<i32> = file_lines.into_iter()
-		.map(|v| v.unwrap().parse().unwrap())
-		.collect::<Vec<i32>>();
+    let mut file_lines = io::stdin().lines();
+    let n: usize = file_lines
+        .next()
+        .expect("value of file")
+        .unwrap()
+        .parse()
+        .unwrap();
+    let values: Vec<i32> = file_lines
+        .into_iter()
+        .map(|v| v.unwrap().parse().unwrap())
+        .collect::<Vec<i32>>();
 
-	//println!("n: {}\nvalues:{:?}",n, values);	
+    let mut ordered = values.clone();
+    ordered.sort_by(|a, b| b.cmp(a));
+    let sched = Scheduling::from(ordered);
 
-	let mut ordered = values.clone();
-	ordered.sort_by(|a, b| b.cmp(a));
-	let max_size = values.iter().sum::<i32>() as usize;
-	let mut min_vec = vec![std::i32::MAX; max_size];
-
-	//println!("max size: {}", max_size);
-	for value_in_iter in ordered.iter() {
-		let size = *value_in_iter;
-
-		let mut i = 0;
-		'nextv:
-		while i < max_size {
-			if let Some(offset) = collision_in_vec(size, i, &mut min_vec){
-				i = offset;
-			}else{
-				for s in 1..=size{
-					min_vec[i] = s;
-					i += 1;
-				}
-				break 'nextv;
-			};
-		}
-	}
-	/* 
-	print!("[");
-	min_vec.iter()
-		.filter(|x| **x != std::i32::MAX)
-		.for_each(|v| print!("{}, ", v));
-	println!("]");
-	*/
-
-	println!("Valor Guloso: {}", min_vec.iter().filter(|x| **x != std::i32::MAX).count());
-	println!("Valor Máximo: {}", max_size);
-	Ok(())
-}
-
-
-fn collision_in_vec(block: i32, i: usize, min_vec: &mut Vec<i32>) -> Option<usize> {
-	//se o tam bloco é menor que o elemento no vetor de indice i
-	if block < min_vec[i] {
-		//calcula se pode colocar os demais valores
-		for i_seg in (i + 1)..(i + block as usize){
-			if block >= min_vec[i_seg]{
-				return Some(i_seg + 1)
-			}
-		}
-	}else{
-		return Some(i + 1)
-	}
-	None
+    println!("Valor Guloso: {}", sched.get_end_time());
+    println!("Valor Máximo: {}", sched.get_trivial_time());
+    println!("Verificacao: {}", sched.verify());
+    Ok(())
 }
---
+++ src/scheduling.rs
@@ -0,0 +1,155 @@
+use std::collections::BTreeMap;
+
+pub type Size = i32;
+
+#[derive(Debug)]
+pub struct Scheduling {
+    collisions: Vec<Size>,
+    end_time: usize,
+    max_size: usize,
+    allocations: BTreeMap<usize, Size>,
+}
+
+impl Scheduling {
+    pub fn new(max_size: usize) -> Scheduling {
+        Scheduling {
+            collisions: vec![Size::MAX; max_size as usize],
+            allocations: BTreeMap::new(),
+            end_time: 0,
+            max_size,
+        }
+    }
+
+    pub fn verify(&self) -> bool {
+        for (start_block, size_first) in &self.allocations {
+            let mut found_eq = false;
+            for (start_other, size_other) in &self.allocations {
+                if start_other == start_block && size_other == size_first && !found_eq {
+                    found_eq = true;
+                    continue;
+                }
+                let distance = start_other.abs_diff(start_block.to_owned()) as i32;
+                let min_size = size_first.min(size_other);
+
+                let is_valid = distance >= min_size.to_owned();
+
+                if !is_valid {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    fn insert_at(&mut self, pos: usize, size: Size) {
+        if self.allocations.contains_key(&pos) {
+            panic!("Tried to add to a position that already contains a task!");
+        }
+        for i in 0..size as usize {
+            self.collisions[i + pos] = (i + 1) as i32
+        }
+        self.allocations.insert(pos, size);
+        let end_time = pos + size as usize;
+        if end_time > self.end_time {
+            self.end_time = end_time;
+        }
+    }
+
+    fn collision_in_vec(&self, block: Size, i: usize) -> Option<usize> {
+        //se o tam bloco é menor que o elemento no vetor de indice i
+
+        if block < self.collisions[i] {
+            //calcula se pode colocar os demais valores
+            for i_seg in (i + 1)..(i + block as usize) {
+                if block >= self.collisions[i_seg] {
+                    return Some(i_seg + 1);
+                }
+            }
+        } else {
+            return Some(i + 1);
+        }
+        None
+    }
+
+    pub fn add(&mut self, block: &Size) -> Option<usize> {
+        let mut pos = 0;
+        while pos < self.max_size {
+            if let Some(next_pos) = self.collision_in_vec(block.to_owned(), pos) {
+                pos = next_pos;
+            } else {
+                self.insert_at(pos, block.to_owned());
+                return Some(pos);
+            }
+        }
+        None
+    }
+
+    pub fn get_end_time(&self) -> usize {
+        self.end_time
+    }
+
+    pub fn get_trivial_time(&self) -> usize {
+        self.max_size
+    }
+}
+
+impl From<Vec<Size>> for Scheduling {
+    fn from(value: Vec<Size>) -> Self {
+        let max_size = value.iter().sum::<i32>() as usize;
+        let mut s = Scheduling::new(max_size);
+        value.iter().for_each(|v| {
+            s.add(v);
+        });
+
+        s
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_empty() {
+        let s = Scheduling::new(10);
+        assert!(s.verify());
+        assert_eq!(s.end_time, 0);
+    }
+
+    #[test]
+    fn finds_not_valid() {
+        let mut s = Scheduling::new(10);
+        s.insert_at(3, 2);
+        s.insert_at(4, 5);
+        assert!(!s.verify());
+        assert_eq!(s.end_time, 9);
+    }
+
+    #[test]
+    fn trivial() {
+        let mut s = Scheduling::new(10);
+        s.insert_at(0, 3);
+        s.insert_at(3, 5);
+        assert!(s.verify());
+    }
+
+    #[test]
+    fn finds_simple() {
+        let mut values = vec![1, 1, 1, 2, 3, 5, 8];
+        values.sort_by(|a, b| b.cmp(a));
+        let sched = Scheduling::from(values);
+        assert_eq!(sched.end_time, 11);
+        assert!(sched.verify());
+        let mut allocation_truth = BTreeMap::new();
+        allocation_truth.insert(0, 8);
+        allocation_truth.insert(1, 1);
+        allocation_truth.insert(2, 2);
+        allocation_truth.insert(3, 1);
+        allocation_truth.insert(4, 1);
+        allocation_truth.insert(5, 5);
+        allocation_truth.insert(8, 3);
+        assert_eq!(sched.allocations, allocation_truth)
+    }
+}
+
