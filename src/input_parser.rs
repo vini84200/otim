009db764dca25af86d49fb89a31d1cee19564cba
Vinicius J. F <www.vini84200@hotmail.com>
2023-08-12 02:57:06 -0300

    Command line arguments

--- src/input_parser.rs
+++ src/input_parser.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{fs::read_to_string, io, path::PathBuf};
 
 use crate::scheduling::Size;
 
@@ -24,5 +24,14 @@ impl InputParser {
 
     pub fn get_values(self) -> Vec<Size> {
         self.sizes
+    }
+
+    pub(crate) fn from_file(file_path: PathBuf) -> InputParser {
+        let file_lines = read_to_string(file_path).expect("Couldnt read the file");
+        let mut file_lines = file_lines.lines();
+        let _n: usize = file_lines.next().expect("value of file").parse().unwrap();
+        let values: Vec<i32> = file_lines.map(|v| v.parse().unwrap()).collect::<Vec<i32>>();
+        Self { sizes: values }
+        // todo!()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
 use simulated_annealing::SimulatedAnnealing;
 
 use crate::{input_parser::InputParser, scheduling::Scheduling};
@@ -8,11 +11,60 @@ mod input_parser;
 mod scheduling;
 mod simulated_annealing;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+
+    #[arg(short, long, value_name = "Input File")]
+    input_file: Option<PathBuf>,
+
+    #[arg(short, long, value_name = "Outpuf FILE")]
+    output_file: Option<PathBuf>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    SimulatedAnnealing {
+        #[arg(short, long, value_name = "Temperatura", default_value_t = 200f64)]
+        temperatura_inicial: f64,
+
+        #[arg(short, long, value_name = "Resfriamente", default_value_t = 0.85f64)]
+        resfriamento: f64,
+
+        #[arg(long, default_value_t = 100)]
+        iterstocool: usize,
+        #[arg(long, default_value_t = 100)]
+        itermaxmetropoles: usize,
+    },
+    Highs,
+}
+
 fn main() {
-    let parser = InputParser::from_stdin();
+    let cli = Cli::parse();
+    let parser = if let Some(file_path) = cli.input_file {
+        InputParser::from_file(file_path)
+    } else {
+        InputParser::from_stdin()
+    };
+    // let parser = InputParser::from_stdin();
     let values = parser.get_values();
-    let mut sim_anealing = SimulatedAnnealing::new(values, 200f64);
-    sim_anealing.run();
+    match &cli.command {
+        Commands::SimulatedAnnealing {
+            temperatura_inicial,
+            resfriamento,
+            itermaxmetropoles,
+            iterstocool,
+        } => {
+            let mut sim_anealing = SimulatedAnnealing::new(values, *temperatura_inicial);
+            sim_anealing.set_resfriamento(*resfriamento);
+            sim_anealing.set_iterstocool(*iterstocool);
+            sim_anealing.set_itermaxmetropoles(*itermaxmetropoles);
+            sim_anealing.run();
+        }
+        Commands::Highs => todo!(),
+    }
 
     //algoritmo de metrópolis de metrópolis em ordem aleatória - inerente a vizinhança
     //processa os s' vizinhos de s
