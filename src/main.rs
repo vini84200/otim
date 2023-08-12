db8cdea5227cd6961853813fc6c362c8b7d0b2a0
Pedro Colle <pedro.h.b.colle@gmail.com>
2023-08-11 23:22:04 -0300

    nova versão do scheduler

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,91 @@
-use itertools::Itertools;
 use std::{io, time::Instant};
+use rand::Rng;
 
 use crate::{input_parser::InputParser, scheduling::Scheduling};
 
+use crate::scheduling_lean::SchedLean;
+
 mod input_parser;
 mod scheduling;
+mod scheduling_lean;
+
+const ITERMAX: usize = 1000000; 
+
+fn gen_vec_n_shuffle(v_in: &Vec<i32>, n: usize) -> Vec<i32>{
+	let mut v = v_in.clone();
+	let mut rng = rand::thread_rng();
+	let last_indice = v.len() - 1;
+	for offset in 0..n{
+		let indice = rng.gen_range(0..=last_indice);
+		v.swap(indice, last_indice - offset);
+	}
+	v
+}
+
+fn solve_with_schedlean(mut best_solution_in: Vec<i32>){
+	let max_val: usize = best_solution_in.iter().sum::<i32>() as usize;
+	let mut solver = SchedLean::new(max_val);
+	
+	let mut best_one = solver.solve(&best_solution_in);
+	
+    let now = Instant::now();
+	for _ in 0..ITERMAX{
+		let testing_solution = gen_vec_n_shuffle(&best_solution_in, 5);
+        let tes_val = solver.solve(&testing_solution);
+		if tes_val < best_one {
+			best_one = tes_val;
+			best_solution_in = testing_solution;
+		}
+	}
+
+	let verifier = Scheduling::from(best_solution_in.clone());
+	if !verifier.verify(){
+		panic!("Valor encontrado não é válido")
+	}
+
+    let duration = now.elapsed();
+    println!("Tempo {:?}", duration);
+
+    //println!("Final one: {}", best_one);
+
+    let duration = now.elapsed();
+    println!("Tempo {:?}", duration);
+
+    println!("Value: {}", best_one);
+    println!("Final one: {:?}", best_solution_in);
+}
+
+fn solve_with_scheduler(mut best_solution_in: Vec<i32>){
+    let mut best_one = Scheduling::from(best_solution_in.clone());
+	
+    let now = Instant::now();
+	for _ in 0..ITERMAX{
+		let testing_solution = gen_vec_n_shuffle(&best_solution_in, 5);
+        let tes_val = Scheduling::from(testing_solution.clone());
+		if tes_val.get_end_time() < best_one.get_end_time() {
+			best_one = tes_val;
+			best_solution_in = testing_solution;
+		}
+	}
+
+	if !best_one.verify(){
+		panic!("Valor encontrado não é válido")
+	}
+
+    let duration = now.elapsed();
+    println!("Tempo {:?}", duration);
+
+    println!("Final one: {}", best_one.get_end_time());
+    println!("Final one: {:?}", best_solution_in);
+}
 
 fn main() -> io::Result<()> {
     let parser = InputParser::from_stdin();
     let values = parser.get_values();
-    let mut ordered = values.clone();
-    let now = Instant::now();
-    ordered.sort_by(|a, b| b.cmp(a));
-    let sched = Scheduling::from(ordered);
 
-    println!("Valor Guloso: {}", sched.get_end_time());
-    println!("Valor Máximo: {}", sched.get_trivial_time());
-    println!("Verificacao: {}", sched.verify());
-    let mut best_one = sched;
-    let mut count: usize = 0;
-
-    println!("Testando permutações:");
-    for perm in values.iter().permutations(values.len()).unique() {
-        // println!("{:?}", perm);
-        let sched = Scheduling::from(perm.to_owned());
-        if sched.verify() {
-            if sched.get_end_time() < best_one.get_end_time() {
-                println!("NOVA Solução {} na iter {count}", sched.get_end_time());
-                best_one = sched;
-            }
-        } else {
-            panic!("Should be valid!! {:?}", sched);
-        }
-        if count % 100000 == 0 {
-            println!("Iter. {}", count);
-            if count % 1000000 == 0 {
-                println!("Best: {}", best_one);
-            }
-        }
-
-        count += 1;
-    }
-    let duration = now.elapsed();
-
-    println!("Final one: {}", best_one);
-    println!("Total de iter. {count}");
-    println!("Tempo {:?}", duration);
-    println!("Value: {}", best_one.get_end_time());
+    let mut best_solution_in = values.clone();
+    best_solution_in.sort_by(|a, b| b.cmp(a));
+	solve_with_schedlean(best_solution_in.clone());
+	solve_with_scheduler(best_solution_in);
     Ok(())
 }
---
+++ src/scheduling_lean.rs
@@ -0,0 +1,102 @@
+use std::{ptr, ops::Add};
+
+pub type Size = u8;
+
+pub struct SchedLean {
+    collisions: Vec<Size>,
+	max_size: usize,
+	end_time: usize
+}
+
+impl SchedLean{
+	pub fn new(max_size: usize) -> Self{
+		SchedLean {
+			collisions: vec![Size::MAX; max_size],
+			max_size,
+			end_time: max_size
+		}
+	}
+	pub fn solve(&mut self, input: &Vec<i32>) -> usize {
+		self.reset();
+		input.iter().for_each(|v| self.add(*v as Size));
+        self.end_time
+	}
+	fn reset(&mut self){
+		self.end_time = 0;
+		self.collisions.fill(Size::MAX);
+	}
+
+    fn insert_at(&mut self, pos: usize, size: Size) {
+        for i in 0..size as usize {
+            self.collisions[i + pos] = (i + 1) as u8 
+        }
+        let end_time = pos + size as usize;
+        if end_time > self.end_time {
+            self.end_time = end_time;
+        }
+    }
+
+    fn collision_in_vec(&self, block: Size, i: usize) -> Option<usize> {
+        if block < self.collisions[i] {
+            for i_seg in (i + 1)..(i + block as usize) {
+                if block >= self.collisions[i_seg] {
+                    return Some(i_seg + 1);
+                }
+            }
+        } else {
+            return Some(i + 1);
+        }
+        None
+    }
+
+    pub fn add(&mut self, block: Size){
+        let mut pos = 0;
+        while pos < self.max_size {
+            if let Some(next_pos) = self.collision_in_vec(block, pos) {
+                pos = next_pos;
+            } else {
+                return self.insert_at(pos, block);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+	use crate::scheduling::Scheduling;
+
+    #[test]
+    fn trivial() {
+		let input = vec![3, 5];
+		let max_size = input.iter().sum::<i32>() as usize;
+		let mut solver = SchedLean::new(max_size);
+		let res = solver.solve(&input);
+        let solution = Scheduling::from(input);
+
+		assert_eq!(max_size, solution.get_trivial_time());
+        assert_eq!(res, solution.get_end_time());
+    }
+
+	/*
+    #[test]
+    fn finds_simple() {
+        let mut values = vec![1, 1, 1, 2, 3, 5, 8];
+        values.sort_by(|a, b| b.cmp(a));
+        let sched = Scheduling::from(values);
+        assert_eq!(sched.end_time, 11);
+        assert!(sched.verify());
+		let mut solution_vec = sched.get_solution_vec();
+		solution_vec.sort_by(|(a, _), (b, _)| a.cmp(b));
+        let mut allocation_truth = Vec::new();
+        allocation_truth.push((0, 8));
+        allocation_truth.push((1, 1));
+        allocation_truth.push((2, 2));
+        allocation_truth.push((3, 1));
+        allocation_truth.push((4, 1));
+        allocation_truth.push((5, 5));
+        allocation_truth.push((8, 3));
+
+        assert_eq!(solution_vec, allocation_truth)
+    } */
+}
